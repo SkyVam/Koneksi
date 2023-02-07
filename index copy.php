79b3057511221dbfa1df19f982489e4197feeefe
SkyVam <yuskaputravito@gmail.com>
2023-02-07 10:37:08 +0700

    Edit 1 file

--- index copy.php
+++ index copy.php
@@ -25,3 +25,17 @@ if (!$db) {
             <button type="submit" name="input" value="Simpan" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> Simpan</button>
     </div>
 </form>
+
+<form action="proses.php"  name="formsuratmasuk" method="post" enctype="multipart/form-data" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+    <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" >Nomor Buku <span class="required">*</span>
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                            <input type="text" id="id_buku" name="id_buku" required="required" placeholder="Masukkan Asal/Pengirim Surat" class="form-control col-md-7 col-xs-12">
+                            </div>
+    </div>                        
+
+    <div class="form-group">
+            <button type="submit" name="input" value="Simpan" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> Simpan</button>
+    </div>
+</form>
